f3c3fe6fb128d4f50e083505072910d2bda7b571
Diorgenes Morais <diorgenesmorais@gmail.com>
2018-01-31 14:13:20 -0300

    add Location on header by event

---
+++ src/main/java/com/dms/event/listener/ResourceCreatedListener.java
@@ -0,0 +1,34 @@
+package com.dms.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.dms.event.ResourceCreatedEvent;
+
+/**
+ * This class has some dependencies that were inserted on the pom.xml
+ * 
+ * @see https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
+ * 
+ * @author Diorgenes Morais
+ * 
+ */
+public class ResourceCreatedListener implements ApplicationListener<ResourceCreatedEvent> {
+
+	@Override
+	public void onApplicationEvent(ResourceCreatedEvent event) {
+		HttpServletResponse response = event.getResponse();
+		Long id = event.getId();
+
+		addHeaderLocation(response, id);
+	}
+
+	private void addHeaderLocation(HttpServletResponse response, Long id) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(id).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+	}
+}
